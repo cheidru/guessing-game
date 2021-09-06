a306ee699c71184a614da17cd2d6a16de965e015
Igor Checheurov <chei@yandex.ru>
2021-09-06 19:03:43 +0300

    init: add code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        let start = 0;
+        let end = 0;
+        let first = 0;
+        let last = 0;
+        let result = 0;
+        let candidate = 0;
+        let middle = 0;
+    }
 
     setRange(min, max) {
-
+        this.start = min;
+        this.end = max;
     }
 
     guess() {
-
+        this.middle = Math.round(this.start + (this.end - this.start) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.end = this.middle;
     }
 
     greater() {
-
+        this.start = this.middle;
     }
 }
 
